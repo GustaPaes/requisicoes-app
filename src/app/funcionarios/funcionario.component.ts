97992fa3e90c204ace0fc9ffe87c3198eae53cb3
gustavo paes de liz <gustavinpaes007@hotmail.com>
2022-09-05 15:28:18 -0300

    Cadastro de Funcionarios Fire Base Auth

--- src/app/funcionarios/funcionario.component.ts
+++ src/app/funcionarios/funcionario.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from '../auth/services/authentication.service';
 import { Departamento } from '../departamentos/models/departamento.model';
 import { DepartamentoService } from '../departamentos/services/departamento.service';
 import { Funcionario } from './models/funcionario.model';
@@ -19,6 +21,8 @@ export class FuncionarioComponent implements OnInit {
   public form: FormGroup;
 
   constructor(
+    private router: Router,
+    private authService: AuthenticationService,
     private funcionarioService: FuncionarioService,
     private departamentoService: DepartamentoService,
     private toastrService: ToastrService,
@@ -28,12 +32,15 @@ export class FuncionarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      id: new FormControl(""),
-      nome: new FormControl("", [Validators.required, Validators.minLength(3)]),
-      email: new FormControl("", [Validators.required, Validators.email]),
-      funcao: new FormControl("", [Validators.required, Validators.minLength(3)]),
-      departamentoId: new FormControl("", [Validators.required]),
-      departamento: new FormControl(""),
+      funcionario: new FormGroup({
+        id: new FormControl(""),
+        nome: new FormControl("", [Validators.required, Validators.minLength(3)]),
+        email: new FormControl("", [Validators.required, Validators.email]),
+        funcao: new FormControl("", [Validators.required, Validators.minLength(3)]),
+        departamentoId: new FormControl("", [Validators.required]),
+        departamento: new FormControl(""),
+      }),
+      senha: new FormControl("")
     });
 
     this.funcionarios$ = this.funcionarioService.selecionarTodos();
@@ -45,23 +52,27 @@ export class FuncionarioComponent implements OnInit {
   }
 
   get id(): AbstractControl | null {
-    return this.form.get("id");
+    return this.form.get("funcionario.id");
   }
 
   get nome(): AbstractControl | null {
-    return this.form.get("nome");
+    return this.form.get("funcionario.nome");
   }
 
   get email(): AbstractControl | null {
-    return this.form.get("email");
+    return this.form.get("funcionario.email");
   }
 
   get funcao(): AbstractControl | null {
-    return this.form.get("funcao");
+    return this.form.get("funcionario.funcao");
   }
 
   get departamentoId(): AbstractControl | null {
-    return this.form.get("departamentoId");
+    return this.form.get("funcionario.departamentoId");
+  }
+
+  get senha(): AbstractControl | null {
+    return this.form.get("senha");
   }
 
   public async gravar(modal: TemplateRef<any>, funcionario?: Funcionario) {
@@ -76,7 +87,7 @@ export class FuncionarioComponent implements OnInit {
         departamento
       }
 
-      this.form.setValue(funcionarioCompleto);
+      this.form.get("funcionario")?.setValue(funcionarioCompleto);
     }
 
     try {
@@ -85,12 +96,18 @@ export class FuncionarioComponent implements OnInit {
       if (this.form.dirty && this.form.valid) {
         if (!funcionario)
         {
-          await this.funcionarioService.inserir(this.form.value)
+          await this.authService.cadastrar(this.email?.value, this.senha?.value);
+
+          await this.funcionarioService.inserir(this.form.get("funcionario")?.value)
           this.toastrService.success("O funcionário foi inserido com sucesso", "Inserção de Funcionários");
+
+          await this.authService.logout();
+
+          await this.router.navigate(["/login"]);
         }
         else
         {
-          await this.funcionarioService.editar(this.form.value);
+          await this.funcionarioService.editar(this.form.get("funcionario")?.value);
           this.toastrService.success("O funcionário foi editado com sucesso", "Edição de Funcionários");
         }
       }
